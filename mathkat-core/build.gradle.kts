d971e20dd5fbcaa693e162e9cdae1ed242dd63c8
Michael Whapples <mwhapples@aim.com>
2022-05-30 17:45:29 +0100

    Gradle build for publishing to sonatype OSSRH

--- mathkat-core/build.gradle.kts
+++ mathkat-core/build.gradle.kts
@@ -8,6 +8,8 @@
 plugins {
     kotlin("jvm") version "1.6.21"
     id("fr.stardustenterprises.rust.importer") version "3.2.1"
+    `maven-publish`
+    signing
 }
 
 dependencies {
@@ -21,7 +23,68 @@ tasks.test {
     useJUnitPlatform()
     systemProperty("onl.mdw.mathkat.rulesDir", File("$rootDir/MathCAT/Rules").absolutePath)
 }
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
 rustImport {
     baseDir.set("/META-INF/native")
     layout.set("hierarchical")
+}
+
+publishing {
+    repositories {
+        if (version.toString().endsWith("SNAPSHOT")) {
+            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+                name = "sonatypeReleaseRepository"
+                credentials(PasswordCredentials::class)
+            }
+        } else {
+            maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+                name = "sonatypeSnapshotRepository"
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("Maven") {
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("MathKat")
+                description.set("Java bindings for MathCAT")
+                url.set("https://github.com/mwhapples/MathKat")
+                licenses {
+                    license {
+                        name.set("Mozilla Public License 2.0")
+                        url.set("https://www.mozilla.org/MPL/2.0/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mwhapples")
+                        name.set("Michael Whapples")
+                        email.set("mwhapples@aim.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/mwhapples/MathKat.git")
+                    developerConnection.set("scm:git:ssh://github.com:mwhapples/MathKat.git")
+                    url.set("https://github.com/mwhapples/MathKat")
+                }
+            }
+        }
+    }
+}
+signing {
+    sign(publishing.publications["Maven"])
 }
